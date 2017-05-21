b19bee78ae0d5cad8d2f0dba6410b7d47c175a43
KickAss\abhir <abhirawat1304@gmail.com>
2017-05-21 19:21:52 +0530

    Bit Manipulation Changes

---
+++ src/com/problem/bitmanipulation/ProblemThree.java
@@ -0,0 +1,27 @@
+package com.problem.bitmanipulation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * 
+ * @author abhir 
+ * To generate all the possible subsets for a given set
+ *
+ */
+public class ProblemThree {
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	int n = Integer.parseInt(br.readLine());
+	int i = Integer.parseInt(br.readLine());
+	generateSubset();
+    }
+
+    private static void generateSubset() {
+	
+    }
+
+}
