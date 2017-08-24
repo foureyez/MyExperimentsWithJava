70c11dc32daeeb8d96c37e3e16383ce37b8c2615
arawat <arawat@IN01N00535.icepor.com>
2017-08-25 01:02:50 +0530

    QuickSort

--- src/com/learn/ds/arrays/QuickSort.java
+++ src/com/learn/ds/arrays/QuickSort.java
@@ -1,40 +1,36 @@
 package com.learn.ds.arrays;
 
-public class QuickSort {
+import com.problem.array.ArrayUtil;
 
-	static void printArray(int arr[]) {
-		int n = arr.length;
-		for (int i = 0; i < n; ++i)
-			System.out.print(arr[i] + " ");
-		System.out.println();
-	}
+public class QuickSort {
 
 	public static void main(String[] args) {
 		int arr[] = { 12, 11, 13, 5, 6, 7 };
 
 		System.out.println("Given Array");
-		printArray(arr);
+		ArrayUtil.printArray(arr);
 
 		quickSort(arr, 0, arr.length - 1);
 
 		System.out.println("\nSorted array");
-		printArray(arr);
+		ArrayUtil.printArray(arr);
 	}
 
-	private static void quickSort(int[] arr, int p, int r) {
+	public static void quickSort(int[] arr, int p, int r) {
 		if (p < r) {
 			int q = partition(arr, p, r);
+
 			quickSort(arr, p, q - 1);
 			quickSort(arr, q + 1, r);
 		}
 	}
 
-	private static int partition(int[] arr, int p, int r) {
-		int temp, x = arr[r];
-		int i = p - 1;
+	private static int partition(int[] arr, int low, int high) {
+		int pivot = arr[high];
+		int i = low - 1, j, temp;
 
-		for (int j = p; j < r; j++) {
-			if (arr[j] <= x) {
+		for (j = low; j < high; j++) {
+			if (arr[j] < pivot) {
 				i++;
 				temp = arr[i];
 				arr[i] = arr[j];
@@ -42,9 +38,9 @@ public class QuickSort {
 			}
 		}
 
-		temp = arr[i + 1];
-		arr[i + 1] = arr[r];
-		arr[r] = temp;
+		temp = arr[high];
+		arr[high] = arr[i + 1];
+		arr[i + 1] = temp;
 
 		return i + 1;
 	}
